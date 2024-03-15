3043c8d076badfc49241a7bf225326dcd4703dea
Luciano Moren <lucianomorenandre03@gmail.com>
2024-03-15 10:10:32 -0300

    Changes in skills

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import style from "./skills.module.css";
 import {
-  SiAdobeillustrator,
   SiExpress,
-  SiPostgresql,
+  SiJavascript,
   SiRedux,
-  SiSequelize,
   SiTailwindcss,
-  SiVisualstudiocode,
 } from "react-icons/si";
 
-import { FaCss3, FaFigma, FaHtml5, FaNode, FaReact } from "react-icons/fa";
-import { IoLogoJavascript } from "react-icons/io";
+import { FaGitAlt, FaNodeJs, FaReact, FaServer, FaTools } from "react-icons/fa";
+import { MdOutlineComputer } from "react-icons/md";
 
 import logo from "../../assets/logo.png";
 import imageSkills from "../../assets/imageskills.png";
-
+import cssLogo from "../../assets/tecnologias/css.png";
+import figmaLogo from "../../assets/tecnologias/figma.png";
+import htmlLogo from "../../assets/tecnologias/html.png";
+import sequelizeLogo from "../../assets/tecnologias/sequelize.png";
+import postgreLogo from "../../assets/tecnologias/postgre.png";
+import ilustratorLogo from "../../assets/tecnologias/ilustrator.png";
+import visualLogo from "../../assets/tecnologias/visual.png";
 function Skills() {
   return (
     <section className={style.skills} id="skills">
@@ -26,51 +29,87 @@ function Skills() {
 
         <div className={style.contenedor}>
           <div className={style.containerSkills}>
-            <div className={style.skill}>
-              <FaHtml5 className={style.iconoSkill} />
-            </div>
-            <div className={style.skill}>
-              <FaCss3 className={style.iconoSkill} />
-            </div>
-            <div className={style.skill}>
-              <SiTailwindcss className={style.iconoSkill} />
-            </div>
-            <div className={style.skill}>
-              <IoLogoJavascript className={style.iconoSkill} />
-            </div>
-            <div className={style.skill}>
-              <FaReact className={style.iconoSkill} />
-            </div>
-            <div className={style.skill}>
-              <SiRedux className={style.iconoSkill} />
-            </div>
-            <div className={style.skill}>
-              <FaNode className={style.iconoSkill} />
-            </div>
-            <div className={style.skill}>
-              <SiExpress className={style.iconoSkill} />
-            </div>
-            <div className={style.skill}>
-              <SiSequelize className={style.iconoSkill} />
-            </div>
-            <div className={style.skill}>
-              <SiPostgresql className={style.iconoSkill} />
-            </div>
-            <div className={style.skill}>
-              <SiVisualstudiocode className={style.iconoSkill} />
-            </div>
-            <div className={style.skill}>
-              <SiAdobeillustrator className={style.iconoSkill} />
+            <div className={style.sectionContainer}>
+              <span>Front End</span>
+              <div className={style.frontEnd}>
+                <div className={style.skill}>
+                  <img src={htmlLogo} alt="htmllogo" />
+                  <p>Html</p>
+                </div>
+                <div className={style.skill}>
+                  <img src={cssLogo} alt="cssLogo" />
+                  <p>Css</p>
+                </div>
+                <div className={style.skill}>
+                  <SiJavascript className={style.js} />
+                  <p>Javascript</p>
+                </div>
+                <div className={style.skill}>
+                  <SiTailwindcss className={style.tailwind} />
+                  <p>Tailwind</p>
+                </div>
+                <div className={style.skill}>
+                  <FaReact className={style.react} />
+                  <p>React</p>
+                </div>
+                <div className={style.skill}>
+                  <SiRedux className={style.redux} />
+                  <p>Redux</p>
+                </div>
+                <div className={style.skill}>
+                  <img src={figmaLogo} alt="figmaLogo" />
+                  <p>Figma</p>
+                </div>
+              </div>
+              <MdOutlineComputer className={style.tools} />
             </div>
 
-            <div className={style.skill}>
-              <FaFigma className={style.iconoSkill} />
+            <div className={style.sectionContainer}>
+              <span>Back End</span>
+              <div className={style.backEnd}>
+                <div className={style.skill}>
+                  <FaNodeJs className={style.node} />
+                  <p>Node</p>
+                </div>
+                <div className={style.skill}>
+                  <SiExpress className={style.express} />
+                  <p>Express</p>
+                </div>
+                <div className={style.skill}>
+                  <img src={sequelizeLogo} alt="sequelizeLogo" />
+                  <p>Sequelize</p>
+                </div>
+                <div className={style.skill}>
+                  <img src={postgreLogo} alt="postgreLogo" />
+                  <p>PostgreSQL</p>
+                </div>
+              </div>
+              <FaServer className={style.tools} />
+            </div>
+            <div className={style.sectionContainer}>
+              <span>Tools</span>
+              <div className={style.herramientas}>
+                <div className={style.skill}>
+                  <img src={visualLogo} alt="visualLogo" />
+                  <p>VS Code</p>
+                </div>
+                <div className={style.skill}>
+                  <img src={ilustratorLogo} alt="ilustratorLogo" />
+                  <p>Illustrator</p>
+                </div>
+
+                <div className={style.skill}>
+                  <FaGitAlt className={style.git} />
+                  <p>Git</p>
+                </div>
+              </div>
+              <FaTools className={style.tools} />
             </div>
           </div>
 
-          <div className={style.imgSkills}>
+          {/* <div className={style.imgSkills}>
             <img src={imageSkills} alt="imagen-skills" />
-          </div>
+          </div> */}
         </div>
       </div>
       <img src={logo} alt="logo-bg" className={style.logoBg}></img>
